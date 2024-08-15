ff5b47efe9e4b963d4a7d62ef0e9e64c49b8da5c
riku546 <opa9788@gmail.com>
2024-08-15 10:57:42 +0900

    数学の問題で無意識に行っていた操作を言語化して、プログラムに落とし込む　むずい

--- atcoder/coins.py
+++ atcoder/coins.py
@@ -1,2 +1,10 @@
-def coins (x):
-  
+def coins (x , A  , B , C):
+  count = 0
+  for i in range(A + 1):
+    for j in range(B + 1):
+      for s in range(C + 1):
+        result = (500 * i ) + (100 * j) + (50 * s)
+        if result == x: count+=1
+  return count
+
+print(coins(100 , 2 , 2 ,2))
