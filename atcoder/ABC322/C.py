8204a261469ccfcee08a2a9ecaa5067cb9ea5f38
riku546 <opa9788@gmail.com>
2024-08-16 04:18:08 +0900

    ABC322 C

---
+++ atcoder/ABC322/C.py
@@ -0,0 +1,13 @@
+# def festival(N , M , A):
+#   for i in range(1 ,N + 1):
+#     for j in range(M):
+#       if i <= A[j]:
+#         print(A[j] - i)
+#         break
+
+# # A =[2, 3]
+# import random
+# M = 200000
+# N = 200000
+# A = [random.randint(1 , M) for i in range(N)]
+# festival(M , N , A) 
--- atcoder/ABC366/C.py
+++ atcoder/ABC366/C.py
@@ -1,47 +1,47 @@
-def minimum_glutton(A, B, X, Y, N):
-    sorted_A = []
-    sorted_B = []
-    temp_A = 0
-    temp_B = 0
-    count_A = 0
-    count_B = 0
+# def minimum_glutton(A, B, X, Y, N):
+#     sorted_A = []
+#     sorted_B = []
+#     temp_A = 0
+#     temp_B = 0
+#     count_A = 0
+#     count_B = 0
     
-    sorted_A = selection_sort(A)
-    sorted_B = selection_sort(B)
-    for i in range(len(sorted_A)):
-      temp_A += sorted_A[i]
-      if temp_A > X:
-        count_A = i + 1
-        break
+#     sorted_A = selection_sort(A)
+#     sorted_B = selection_sort(B)
+#     for i in range(len(sorted_A)):
+#       temp_A += sorted_A[i]
+#       if temp_A > X:
+#         count_A = i + 1
+#         break
       
-    for i in range(len(sorted_B)):
-      temp_B += sorted_B[i]
-      if temp_B > Y:
-        count_B = i + 1
-        break
+#     for i in range(len(sorted_B)):
+#       temp_B += sorted_B[i]
+#       if temp_B > Y:
+#         count_B = i + 1
+#         break
       
-    return min(count_B ,count_A)
+#     return min(count_B ,count_A)
 
 
-def selection_sort(nums):
-    for i in range(len(nums) - 1, -1, -1):
-        temp = i
-        for j in range(i - 1, -1, -1):
-            if nums[temp] > nums[j]:
-                temp = j
-        nums[i], nums[temp] = nums[temp], nums[i]
+# def selection_sort(nums):
+#     for i in range(len(nums) - 1, -1, -1):
+#         temp = i
+#         for j in range(i - 1, -1, -1):
+#             if nums[temp] > nums[j]:
+#                 temp = j
+#         nums[i], nums[temp] = nums[temp], nums[i]
 
-    return nums
-
-
+#     return nums
 
 
-A = [2, 3, 5, 1]
-B = [8, 8, 1, 4]
 
-# A = [1 ,1 ,1 ,1 ,1]
-# B = [2 ,2 ,2 ,2 ,2]
 
-A=[1,2,3,4,5,6,7,8]
-B= [8,7,6,5,4,3,2,1]
-print(minimum_glutton(A, B, 30,30 , 6))
+# A = [2, 3, 5, 1]
+# B = [8, 8, 1, 4]
+
+# # A = [1 ,1 ,1 ,1 ,1]
+# # B = [2 ,2 ,2 ,2 ,2]
+
+# A=[1,2,3,4,5,6,7,8]
+# B= [8,7,6,5,4,3,2,1]
+# print(minimum_glutton(A, B, 30,30 , 6))
