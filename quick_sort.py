8808486f27de037a2524399bd9b416e7a4babffc
riku546 <opa9788@gmail.com>
2024-08-19 03:47:50 +0900

    再帰関数は呼び出した処理が終わったら、もとの呼び出し元にもどる

--- quick_sort.py
+++ quick_sort.py
@@ -70,9 +70,34 @@
 #     return nums
 
 
-# if __name__ == "__main__":
-#     import random
+def partition(nums, low, high):
+    i = low - 1
+    pivot = nums[high]
 
-#     nums = [8, 7, 6, 1, 0, 9, 2]
-#     nums = [random.randint(0, 1000) for i in range(100000)]
-#     print(quick_sort(nums, 0, len(nums) - 1))
+    for j in range(low, high):
+        if nums[j] <= pivot:
+            i += 1
+            nums[j], nums[i] = nums[i], nums[j]
+
+    i += 1
+    nums[i], nums[high] = nums[high], nums[i]
+
+    return i
+
+
+def quick_sort(nums, low, high):
+    if low < high:
+        print(low , high)
+        partition_index = partition(nums, low, high)
+        quick_sort(nums, low, partition_index - 1)
+        quick_sort(nums, partition_index + 1, high)
+
+    return nums
+
+
+if __name__ == "__main__":
+    import random
+
+    nums = [8, 7, 6, 1, 0, 9, 2]
+    # nums = [random.randint(0, 1000) for i in range(100000)]
+    print(quick_sort(nums, 0, len(nums) - 1))
