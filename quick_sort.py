d7769d3e6e9547b6c322959e6759b640153ed11a
riku546 <opa9788@gmail.com>
2024-08-18 12:11:15 +0900

    quick sort

--- quick_sort.py
+++ quick_sort.py
@@ -46,12 +46,33 @@
 # #     return nums
 
 
+def partition(nums, low, high):
+    i = low - 1
+    pivot = nums[high]
 
-# if __name__ == "__main__":
-#     import random
+    for j in range(low, high):
+        if pivot >= nums[j]:
+            i += 1
+            nums[j], nums[i] = nums[i], nums[j]
+    i += 1
+    nums[i], nums[high] = nums[high], nums[i]
 
-#     # nums = [1, 8, 3, 9, 4, 5, 7]
-#     nums = [8, 7, 6, 1, 0, 9, 2]
-#     # nums = [random.randint(0, 1000) for i in range(10000)]
-#     # print(sort_processor(nums))
-#     print(quick_sort(nums, 0, len(nums) - 1))
+    return i
+
+
+def quick_sort(nums, low, high):
+    if low < high:
+        partition_index = partition(nums, low, high)
+        quick_sort(nums, low, partition_index - 1)
+        quick_sort(nums, partition_index + 1, high)
+
+    return nums
+
+
+if __name__ == "__main__":
+    import random
+
+    nums = [8, 7, 6, 1, 0, 9, 2]
+    # nums = [random.randint(0, 1000) for i in range(10000)]
+    # print(sort_processor(nums))
+    print(quick_sort(nums, 0, len(nums) - 1))
