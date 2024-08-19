5ad38f0b0cdead81b4b1773cc8bb2ec1a757bc4f
riku546 <opa9788@gmail.com>
2024-08-19 17:16:39 +0900

    merge sort

--- quick_sort.py
+++ quick_sort.py
@@ -70,34 +70,42 @@
 #     return nums
 
 
-def partition(nums, low, high):
-    i = low - 1
-    pivot = nums[high]
+# def partition(nums, low, high):
+#     i = low - 1
+#     pivot = nums[high]
 
-    for j in range(low, high):
-        if nums[j] <= pivot:
-            i += 1
-            nums[j], nums[i] = nums[i], nums[j]
+#     for j in range(low, high):
+#         if nums[j] <= pivot:
+#             i += 1
+#             nums[j], nums[i] = nums[i], nums[j]
 
-    i += 1
-    nums[i], nums[high] = nums[high], nums[i]
+#     i += 1
+#     nums[i], nums[high] = nums[high], nums[i]
 
-    return i
-
-
-def quick_sort(nums, low, high):
-    if low < high:
-        print(low , high)
-        partition_index = partition(nums, low, high)
-        quick_sort(nums, low, partition_index - 1)
-        quick_sort(nums, partition_index + 1, high)
-
-    return nums
+#     return i
 
 
-if __name__ == "__main__":
-    import random
+# def quick_sort(nums, low, high):
+#     if low < high:
+#         partition_index = partition(nums, low, high)
+#         quick_sort(nums, low, partition_index - 1)
+#         quick_sort(nums, partition_index + 1, high)
 
-    nums = [8, 7, 6, 1, 0, 9, 2]
-    # nums = [random.randint(0, 1000) for i in range(100000)]
-    print(quick_sort(nums, 0, len(nums) - 1))
+#     return nums
+
+# def bubble_sort(nums):
+#   len_nums = len(nums)
+#   for i in range(len_nums):
+#     for j in range(len_nums - 1):
+#       if nums[j] > nums[j + 1]:
+#         nums[j] , nums[j + 1] = nums[j + 1] , nums[j]
+#   return nums
+
+
+# if __name__ == "__main__":
+#     import random
+
+#     nums = [8, 7, 6, 1, 0, 9, 2]
+#     nums = [random.randint(0, 1000) for i in range(100000)]
+#     print(quick_sort(nums, 0, len(nums) - 1))
+#     # print(bubble_sort(nums))
