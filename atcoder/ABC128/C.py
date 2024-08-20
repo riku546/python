16ed348dd7c43670588757bf102c151d73414e15
riku546 <opa9788@gmail.com>
2024-08-21 02:40:01 +0900

    ビット演算意味わからん

---
+++ atcoder/ABC128/C.py
@@ -0,0 +1,58 @@
+def bit_search(N, M, K, P):
+    result = []
+    for i in range(1 << M):
+        temp = []
+        for j in range(M):
+            if i & (1 << j):
+                temp.append("ok")
+            else:
+                temp.append("off")
+
+        # result.append(temp)
+
+    return result
+
+
+def count_valid_combinations(n, m, switches, p):
+    valid_count = 0
+
+    # 全てのスイッチの状態の組み合わせをビット全探索
+    for i in range(2**n):
+        is_valid = True
+
+        # 全ての電球に対して点灯条件をチェック
+        for j in range(m):
+            on_count = 0
+            for switch in switches[j]:
+                if i & (1 << (switch - 1)):
+                    on_count += 1
+
+            if on_count % 2 != p[j]:
+                is_valid = False
+                break
+
+        if is_valid:
+            valid_count += 1
+
+    return valid_count
+
+
+def example(nums):
+    result = []
+    n = len(nums)
+    for i in range(1 << n):
+        temp = []
+        for j in range(n):
+            if i & (1 << j):
+                print(bin(i)[2:], bin(1 << j)[2:] , nums[j])
+                temp.append(nums[j])
+
+        result.append(temp)
+    return result
+
+
+if __name__ == "__main__":
+    # example(13, 5)
+    nums = [1, 2, 3]
+    
+    print(example(nums))
