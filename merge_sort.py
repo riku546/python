0d75c1d6d053154669ebf6334d10fcfcea1fb736
riku546 <opa9788@gmail.com>
2024-08-25 00:41:01 +0900

    merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -108,40 +108,78 @@
 #     return nums
 
 
-def merge_sort(nums):
-    if len(nums) <= 1:
-        return nums
+# def merge_sort(nums):
+#     if len(nums) <= 1:
+#         return nums
 
-    center = len(nums) // 2
-    left = nums[:center]
-    right = nums[center:]
+#     center = len(nums) // 2
+#     left = nums[:center]
+#     right = nums[center:]
 
-    merge_sort(left)
-    merge_sort(right)
+#     merge_sort(left)
+#     merge_sort(right)
 
-    i = j = k = 0
+#     i = j = k = 0
 
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            nums[k] = left[i]
-            i += 1
-        else:
-            nums[k] = right[j]
-            j += 1
+#     while i < len(left) and j < len(right):
+#         if left[i] <= right[j]:
+#             nums[k] = left[i]
+#             i += 1
+#         else:
+#             nums[k] = right[j]
+#             j += 1
 
-        k += 1
+#         k += 1
 
-    while i < len(left):
-        nums[k] = left[i]
-        i += 1
-        k += 1
+#     while i < len(left):
+#         nums[k] = left[i]
+#         i += 1
+#         k += 1
 
-    while j < len(right):
-        nums[k] = right[j]
-        j += 1
-        k += 1
+#     while j < len(right):
+#         nums[k] = right[j]
+#         j += 1
+#         k += 1
 
-    return nums
+#     return nums
+
+
+# def merge_sort(nums):
+#     if len(nums) <= 1:
+#         return nums
+
+#     center = len(nums) // 2
+#     left = nums[:center]
+#     right = nums[center:]
+
+#     merge_sort(left)
+#     merge_sort(right)
+
+#     i = j = k = 0
+
+#     while i < len(left) and j < len(right):
+#         if left[i] <= right[j]:
+#             nums[k] = left[i]
+
+#             i += 1
+#             k += 1
+#         else:
+#             nums[k] = right[j]
+
+#             j += 1
+#             k += 1
+
+#     while i < len(left):
+#         nums[k] = left[i]
+#         i += 1
+#         k += 1
+
+#     while j < len(right):
+#         nums[k] = right[j]
+#         j += 1
+#         k += 1
+
+#     return nums
 
 
 if __name__ == "__main__":
