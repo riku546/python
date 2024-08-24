3e0c4cf8667d005752802bd7877bfaf3139186fd
riku546 <opa9788@gmail.com>
2024-08-25 02:47:40 +0900

    学び：問題文で出てくる変数と自分で定義する変数を変える

--- atcoder/ABC356/C.py
+++ atcoder/ABC356/C.py
@@ -1,7 +1,41 @@
-https://atcoder.jp/contests/abc356/tasks/abc356_c
+def test_keys(temp, C, K):
+    for x in range(len(C)):
 
+        count = 0
+
+        for m in range(1, int(C[x][0]) + 1):
+
+            if int(C[x][m]) in temp:
+                count += 1
+
+        if C[x][-1] == "o":
+            if count < K:
+                return False
+
+        elif C[x][-1] == "x":
+            if count >= K:
+                return False
+
+    return True
+
+
+def keys(N, M, K, C):
+    result = 0
+    for i in range(2**N):
+        temp = []
+
+        for j in range(N):
+            if i & (1 << j):
+                temp.append(j + 1)
+
+        if test_keys(temp, C, K):
+            result += 1
+
+    return result
 
 
 if __name__ == "__main__":
-  N , M , K = list(map(int , input().split()))
-  C = [list(input().split()) for i in range(M)]
+    N, M, K = list(map(int, input().split()))
+    C = [list(input().split()) for i in range(M)]
+
+    print(keys(N, M, K, C))
